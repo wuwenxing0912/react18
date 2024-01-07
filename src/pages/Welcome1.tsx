163003daba43fbc498bbf96476f1b38d2387e9b1
wuwenxing0912 <1428560819@qq.com>
2024-01-07 17:30:46 +0800

    css in js scss 多个class处理方式

--- src/pages/Welcome1.tsx
+++ src/pages/Welcome1.tsx
@@ -1,6 +1,20 @@
 import * as React from "react";
 import style from "./Welcome1.module.scss";
+import classNames from "classnames";
 
 export const Welcome1: React.FC = () => {
-  return <div className={style.wrapper}>下一页 scss写法</div>;
+  //[style.wrapper,style.wrapper_text_color,"test_no_random",].join(" ")
+  return (
+    <div>
+      <div
+        className={classNames(
+          style.wrapper,
+          style.wrapper_text_color,
+          "test_no_random"
+        )}
+      >
+        下一页 scss写法
+      </div>
+    </div>
+  );
 };

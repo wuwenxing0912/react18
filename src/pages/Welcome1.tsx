6f1ab162bbb02e8a0a1c7ba0fb296af83b1ac476
wuwenxing0912 <1428560819@qq.com>
2024-01-08 15:05:43 +0800

    重置代码

--- src/pages/Welcome1.tsx
+++ src/pages/Welcome1.tsx
@@ -1,13 +1,12 @@
-import p from "../assets/images/welcome1.svg";
-
+import p from '../assets/images/welcome1.svg'
 export const Welcome1: React.FC = () => {
   return (
     <div text-center>
-      <img src={p} w-128px h-130px />
-      <h2 text-32px mt-48px>
-        会挣钱 <br />
+      <img w-128px h-130px src={p} />
+      <h2 text-32px mt-48px >
+        会挣钱 <br/>
         还要会省钱
       </h2>
     </div>
-  );
-};
+  )
+}
--- src/pages/Welcome2.tsx
+++ src/pages/Welcome2.tsx
@@ -1,13 +1,12 @@
-import p from "../assets/images/welcome2.svg";
-
+import p from '../assets/images/welcome2.svg'
 export const Welcome2: React.FC = () => {
   return (
     <div text-center>
-      <img src={p} w-128px h-150px />
-      <h2 text-32px mt-48px>
+      <img w-128px h-150px src={p} />
+      <h2 text-32px mt-48px >
         每日提醒 <br />
         不会遗漏每一笔账单
       </h2>
     </div>
-  );
-};
+  )
+}

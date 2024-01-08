1525e68fa94a7507683cb3591584bbf3a34fd094
wuwenxing0912 <1428560819@qq.com>
2024-01-08 15:27:28 +0800

    解决ts报错

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -1,52 +1,84 @@
-import { animated, useTransition } from '@react-spring/web'
-import type { ReactNode } from 'react'
-import { useRef, useState } from 'react'
-import { Link, useLocation, useOutlet } from 'react-router-dom'
-import logo from '../assets/images/logo.svg'
-const linkMap = {
-  '/welcome/1': '/welcome/2',
-  '/welcome/2': '/welcome/3',
-  '/welcome/3': '/welcome/4',
-  '/welcome/4': '/welcome/xxx',
-}
+import { animated, useTransition } from "@react-spring/web";
+import type { ReactNode } from "react";
+import { useRef, useState } from "react";
+import { Link, useLocation, useOutlet } from "react-router-dom";
+import logo from "../assets/images/logo.svg";
+const linkMap: Record<string, string> = {
+  "/welcome/1": "/welcome/2",
+  "/welcome/2": "/welcome/3",
+  "/welcome/3": "/welcome/4",
+  "/welcome/4": "/welcome/xxx",
+};
 export const WelcomeLayout: React.FC = () => {
-  const map = useRef<Record<string, ReactNode>>({})
-  const location = useLocation()
-  const outlet = useOutlet()
-  map.current[location.pathname] = outlet
-  const [extraStyle, setExtraStyle] = useState({ position: 'relative' })
+  const map = useRef<Record<string, ReactNode>>({});
+  const location = useLocation();
+  const outlet = useOutlet();
+  map.current[location.pathname] = outlet;
+  const [extraStyle, setExtraStyle] = useState<{
+    position: "relative" | "absolute";
+  }>({ position: "relative" });
   const transitions = useTransition(location.pathname, {
-    from: { transform: location.pathname === '/welcome/1' ? 'translateX(0%)' : 'translateX(100%)' },
-    enter: { transform: 'translateX(0%)' },
-    leave: { transform: 'translateX(-100%)' },
+    from: {
+      transform:
+        location.pathname === "/welcome/1"
+          ? "translateX(0%)"
+          : "translateX(100%)",
+    },
+    enter: { transform: "translateX(0%)" },
+    leave: { transform: "translateX(-100%)" },
     config: { duration: 300 },
     onStart: () => {
-      setExtraStyle({ position: 'absolute' })
+      setExtraStyle({ position: "absolute" });
     },
     onRest: () => {
-      setExtraStyle({ position: 'relative' })
-    }
-  })
+      setExtraStyle({ position: "relative" });
+    },
+  });
   return (
     <div className="bg-#5f34bf" h-screen flex flex-col items-stretch pb-16px>
       <header shrink-0 text-center pt-64px>
         <img src={logo} w-64px h-69px />
-        <h1 text="#D4D4EE" text-32px>山竹记账</h1>
+        <h1 text="#D4D4EE" text-32px>
+          山竹记账
+        </h1>
       </header>
-      <main shrink-1 grow-1 relative >
-        {transitions((style, pathname) =>
-          <animated.div key={pathname} style={{ ...style, ...extraStyle }} w="100%" h="100%" p-16px flex>
-            <div grow-1 bg-white flex justify-center items-center rounded-8px>
-              {map.current[pathname]}
-            </div>
-          </animated.div>
-        )}
+      <main shrink-1 grow-1 relative>
+        {transitions((style, pathname) => (
+          <div style={extraStyle} w="100%" h="100%">
+            <animated.div
+              key={pathname}
+              style={style}
+              w="100%"
+              h="100%"
+              p-16px
+              flex
+            >
+              <div grow-1 bg-white flex justify-center items-center rounded-8px>
+                {map.current[pathname]}
+              </div>
+            </animated.div>
+          </div>
+        ))}
       </main>
-      <footer shrink-0 text-center text-24px text-white grid grid-cols-3 grid-rows-1>
-        <Link style={{ gridArea: '1 / 2 / 2 / 3' }} to={linkMap[location.pathname]}>下一页</Link>
-        <Link style={{ gridArea: '1 / 3 / 2 / 4' }} to="/welcome/xxx">跳过</Link>
+      <footer
+        shrink-0
+        text-center
+        text-24px
+        text-white
+        grid
+        grid-cols-3
+        grid-rows-1
+      >
+        <Link
+          style={{ gridArea: "1 / 2 / 2 / 3" }}
+          to={linkMap[location.pathname]}
+        >
+          下一页
+        </Link>
+        <Link style={{ gridArea: "1 / 3 / 2 / 4" }} to="/welcome/xxx">
+          跳过
+        </Link>
       </footer>
     </div>
-  )
-}
-
+  );
+};

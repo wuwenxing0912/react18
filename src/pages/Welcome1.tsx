4d84446294e6b7b6d33c5d6e81420660e8b489f0
wuwenxing0912 <1428560819@qq.com>
2024-01-08 10:48:03 +0800

    四个欢迎页面

--- src/pages/Welcome1.tsx
+++ src/pages/Welcome1.tsx
@@ -1,9 +1,13 @@
-import { Link } from "react-router-dom";
+import p from "../assets/images/welcome1.svg";
 
 export const Welcome1: React.FC = () => {
   return (
-    <div>
-      1 <Link to="/welcome/2">下一页</Link>
+    <div text-center>
+      <img src={p} />
+      <h2 text-32px mt-48px>
+        会挣钱 <br />
+        还要会省钱
+      </h2>
     </div>
   );
 };

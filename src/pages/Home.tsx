3a9858dad517d477fa5124673e795510c0e2578e
wuwenxing0912 <1428560819@qq.com>
2024-01-09 16:03:58 +0800

    完成首页

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,31 @@
-import axios from "axios";
-import useSWR from "swr";
-
-const getName = (path: string) => {
-  return axios.get<{ name: any }>(path);
-};
+import pig from "../assets/images/pig.svg";
+import add from "../assets/icons/add.svg";
 
 export const Home: React.FC = () => {
-  const { data, error, isValidating } = useSWR("/api/user", getName);
-  console.log(data?.data);
-  if (error) return <div>failed to load</div>;
-  if (isValidating) return <div>loading...</div>;
-  return <div>hello {data?.data.name}!</div>;
+  return (
+    <div>
+      <div flex justify-center items-center>
+        <img src={pig} h-128px w-130px mt-20vh mb-20vh />
+      </div>
+      <div px-16px>
+        <button w="100%" bg="#5C33BE" b-none rounded-8px text-white h-48px>
+          开始记账
+        </button>
+      </div>
+      <button
+        fixed
+        right-16px
+        bottom-16px
+        bg="#5C33BE"
+        w-56px
+        h-56px
+        b-none
+        rounded="50%"
+        p-4px
+        text-6xl
+      >
+        <img src={add} max-w="100%" max-h="100%" />
+      </button>
+    </div>
+  );
 };

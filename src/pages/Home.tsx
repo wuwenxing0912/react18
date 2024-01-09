fd7942eee2121f826d81d04328357d97c1f95027
wuwenxing0912 <1428560819@qq.com>
2024-01-09 16:05:16 +0800

    重置代码

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,18 @@
-import pig from "../assets/images/pig.svg";
-import add from "../assets/icons/add.svg";
-
+import p from '../assets/images/pig.svg'
+import add from '../assets/icons/add.svg'
 export const Home: React.FC = () => {
-  return (
-    <div>
-      <div flex justify-center items-center>
-        <img src={pig} h-128px w-130px mt-20vh mb-20vh />
-      </div>
-      <div px-16px>
-        <button w="100%" bg="#5C33BE" b-none rounded-8px text-white h-48px>
-          开始记账
-        </button>
-      </div>
-      <button
-        fixed
-        right-16px
-        bottom-16px
-        bg="#5C33BE"
-        w-56px
-        h-56px
-        b-none
-        rounded="50%"
-        p-4px
-        text-6xl
-      >
-        <img src={add} max-w="100%" max-h="100%" />
-      </button>
+  return <div>
+    <div flex justify-center items-center>
+      <img mt-20vh mb-20vh width="128" height="130" src={p} />
     </div>
-  );
-};
+    <div px-16px>
+      <button h-48px w="100%" bg="#5C33BE" b-none text-white
+        rounded-8px
+      >开始记账</button>
+    </div>
+    <button p-4px w-56px h-56px bg="#5C33BE" rounded="50%" b-none text-white
+      text-6xl fixed bottom-16px right-16px>
+      <img src={add} max-w="100%" max-h="100%" />
+    </button>
+  </div>
+}
--- src/shims.d.ts
+++ src/shims.d.ts
@@ -1,17 +1,17 @@
-import * as React from "react";
-declare module "react" {
+import * as React from 'react'
+declare module 'react' {
   interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
-    flex?: boolean;
-    relative?: boolean;
-    text?: string;
-    grid?: boolean;
-    before?: string;
-    after?: string;
-    shadow?: boolean;
-    w?: string;
-    h?: string;
-    bg?: string;
-    fixed?: boolean;
-    rounded?: string;
+    flex?: boolean
+    relative?: boolean
+    text?: string
+    grid?: boolean
+    before?: string
+    after?: string
+    shadow?: boolean
+    w?: string
+    h?: string
+    bg?: string
+    rounded?: string
+    fixed?: boolean
   }
 }

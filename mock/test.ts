39e12dad31fa238bc82f8e50a80bb2498990573e
wuwenxing0912 <1428560819@qq.com>
2024-01-12 13:16:26 +0800

    新建组件 Loading\AddItemFloatButton Icon\

--- mock/test.ts
+++ mock/test.ts
@@ -18,27 +18,34 @@ export default [
   {
     url: "/api/v1/items",
     method: "get",
-    timeout: 1000,
     response: (): Resources<Item> => {
       return {
-        resources: [
-          {
-            id: 1,
-            user_id: 1,
-            amount: 1000,
-            tag_ids: [1, 2],
-            happen_at: "2021-08-01T00:00:00.000Z",
-            created_at: "2021-08-01T00:00:00.000Z",
-            updated_at: "2021-08-01T00:00:00.000Z",
-            kind: "expenses",
-          },
-        ],
+        resources: [],
         pager: {
           page: 1,
           per_page: 25,
-          count: 100,
+          count: 0,
         },
       };
+      // return {
+      //   resources: [
+      //     {
+      //       id: 1,
+      //       user_id: 1,
+      //       amount: 1000,
+      //       tag_ids: [1, 2],
+      //       happen_at: "2021-08-01T00:00:00.000Z",
+      //       created_at: "2021-08-01T00:00:00.000Z",
+      //       updated_at: "2021-08-01T00:00:00.000Z",
+      //       kind: "expenses",
+      //     },
+      //   ],
+      //   pager: {
+      //     page: 1,
+      //     per_page: 25,
+      //     count: 100,
+      //   },
+      // };
     },
   },
 ] as MockMethod[];
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,44 +1,47 @@
-import useSWR from 'swr'
-import { Navigate } from 'react-router-dom'
-import p from '../assets/images/pig.svg'
-import add from '../assets/icons/add.svg'
-import { ajax } from '../lib/ajax'
-import { useTitle } from '../hooks/useTitle'
+import useSWR from "swr";
+import { Navigate } from "react-router-dom";
+import p from "../assets/images/pig.svg";
+import add from "../assets/icons/add.svg";
+import { ajax } from "../lib/ajax";
+import { useTitle } from "../hooks/useTitle";
+import { Loading } from "../components/Loading";
+import { AddItemFloatButton } from "../components/AddItemFloatButton";
 interface Props {
-  title?: string
+  title?: string;
 }
 export const Home: React.FC<Props> = (props) => {
-  useTitle(props.title)
-  const { data: meData, error: meError } = useSWR('/api/v1/me', async path =>
-    (await ajax.get<Resource<User>>(path)).data.resource
-  )
-  const { data: itemsData, error: itemsError } = useSWR(meData ? '/api/v1/items' : null, async path =>
-    (await ajax.get<Resources<Item>>(path)).data
-  )
+  useTitle(props.title);
+  const { data: meData, error: meError } = useSWR(
+    "/api/v1/me",
+    async (path) => (await ajax.get<Resource<User>>(path)).data.resource
+  );
+  const { data: itemsData, error: itemsError } = useSWR(
+    meData ? "/api/v1/items" : null,
+    async (path) => (await ajax.get<Resources<Item>>(path)).data
+  );
 
-  const isLoadingMe = !meData && !meError
-  const isLoadingItems = meData && !itemsData && !itemsError
+  const isLoadingMe = !meData && !meError;
+  const isLoadingItems = meData && !itemsData && !itemsError;
 
   if (isLoadingMe || isLoadingItems) {
-    return <div>加载中……</div>
+    return <Loading />;
   }
 
   if (itemsData?.resources[0]) {
-    return <Navigate to="/items" />
+    return <Navigate to="/items" />;
   }
 
-  return <div>
-    <div flex justify-center items-center>
-      <img mt-20vh mb-20vh width="128" height="130" src={p} />
+  return (
+    <div>
+      <div flex justify-center items-center>
+        <img mt-20vh mb-20vh width="128" height="130" src={p} />
+      </div>
+      <div px-16px>
+        <button h-48px w="100%" bg="#5C33BE" b-none text-white rounded-8px>
+          开始记账
+        </button>
+      </div>
+      <AddItemFloatButton />
     </div>
-    <div px-16px>
-      <button h-48px w="100%" bg="#5C33BE" b-none text-white
-        rounded-8px
-      >开始记账</button>
-    </div>
-    <button p-4px w-56px h-56px bg="#5C33BE" rounded="50%" b-none text-white
-      text-6xl fixed bottom-16px right-16px>
-      <img src={add} max-w="100%" max-h="100%" />
-    </button>
-  </div >
-}
+  );
+};

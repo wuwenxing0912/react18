70420ccbe0c4cdd4b4b866ff2e99657e11577296
wuwenxing0912 <1428560819@qq.com>
2024-01-12 13:30:42 +0800

    引入 svgsprites Vite 插件 完善Icon 组件

--- src/components/AddItemFloatButton.tsx
+++ src/components/AddItemFloatButton.tsx
@@ -1,21 +1,22 @@
-import add from "../assets/icons/add.svg";
+import { Icon } from "./Icon";
 
 export const AddItemFloatButton: React.FC = () => {
   return (
     <button
-      p-4px
       w-56px
       h-56px
       bg="#5C33BE"
       rounded="50%"
       b-none
       text-white
-      text-6xl
       fixed
       bottom-16px
       right-16px
+      flex
+      justify-center
+      items-center
     >
-      <img src={add} max-w="100%" max-h="100%" />
+      <Icon name="add" className="w-48px h-48px" />
     </button>
   );
 };
--- src/shims.d.ts
+++ src/shims.d.ts
@@ -1,17 +1,21 @@
-import * as React from 'react'
-declare module 'react' {
+import * as React from "react";
+declare module "react" {
   interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
-    flex?: boolean
-    relative?: boolean
-    text?: string
-    grid?: boolean
-    before?: string
-    after?: string
-    shadow?: boolean
-    w?: string
-    h?: string
-    bg?: string
-    rounded?: string
-    fixed?: boolean
+    flex?: boolean;
+    relative?: boolean;
+    text?: string;
+    grid?: boolean;
+    before?: string;
+    after?: string;
+    shadow?: boolean;
+    w?: string;
+    h?: string;
+    bg?: string;
+    rounded?: string;
+    fixed?: boolean;
+  }
+  interface SVGProps<T> extends SVGAttributes<T>, ClassAttributes<T> {
+    w?: string;
+    h?: string;
   }
 }

306ff01c4be50508fa56626398956e2ac80e402f
wuwenxing0912 <1428560819@qq.com>
2024-01-13 11:35:16 +0800

    完善 Topnav 组件

--- src/components/Topnav.tsx
+++ src/components/Topnav.tsx
@@ -1,3 +1,12 @@
-export const Topnav: React.FC = () => {
-  return <div>Topnav </div>;
+import { Icon } from "./Icon";
+interface Props {
+  title?: string;
+}
+export const Topnav: React.FC<Props> = ({ title = "山竹记账" }) => {
+  return (
+    <div flex items-center p-16px text-white>
+      <Icon name="menu" className="h-24px w-24px mr-16px" />
+      <h1 text-24px>{title}</h1>
+    </div>
+  );
 };

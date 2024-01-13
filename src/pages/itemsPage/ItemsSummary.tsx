8b00378f3e7a111b2cb50cd9c4645382d005b64d
wuwenxing0912 <1428560819@qq.com>
2024-01-13 16:51:38 +0800

    完善 ItemsSummary 组件

--- src/pages/itemsPage/ItemsSummary.tsx
+++ src/pages/itemsPage/ItemsSummary.tsx
@@ -1,3 +1,29 @@
 export const ItemsSummary: React.FC = () => {
-  return <div>ItemsSummary </div>;
+  return (
+    <ol
+      flex
+      justify-between
+      items-center
+      m-16px
+      py-12px
+      px-16px
+      bg="#252A23"
+      rounded-8px
+      text-center
+      children-px-24px
+    >
+      <li text="#FE7275">
+        <div>收入</div>
+        <div>1000</div>
+      </li>
+      <li text="#53A867">
+        <div>支出</div>
+        <div>1000</div>
+      </li>
+      <li text-white>
+        <div>净收入</div>
+        <div>1000</div>
+      </li>
+    </ol>
+  );
 };

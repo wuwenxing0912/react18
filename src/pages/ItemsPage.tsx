4c135c178572138d09ded6d4161bf8d4c81d1e24
wuwenxing0912 <1428560819@qq.com>
2024-01-13 11:17:19 +0800

    创建ItemsPage

---
+++ src/pages/ItemsPage.tsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+import { AddItemFloatButton } from "../components/AddItemFloatButton";
+import { TimeRangePicker } from "../components/TimeRangePicker";
+import { Topnav } from "../components/Topnav";
+import { ItemsList } from "./itemsPage/ItemsList";
+import { ItemsSummary } from "./itemsPage/ItemsSummary";
+
+const Div = styled.div`
+  background: linear-gradient(
+    0deg,
+    rgba(143, 76, 215, 1) 0%,
+    rgba(92, 51, 190, 1) 100%
+  );
+`;
+
+export const ItemsPage: React.FC = () => {
+  return (
+    <div>
+      <Div>
+        <Topnav />
+        <TimeRangePicker />
+      </Div>
+      <ItemsSummary />
+      <ItemsList />
+      <AddItemFloatButton />
+    </div>
+  );
+};

8439fc9e2dfa05562664ab4c3387ebba587648f4
wuwenxing0912 <1428560819@qq.com>
2024-01-16 11:29:19 +0800

    完善TopNav组件

--- src/components/Topnav.tsx
+++ src/components/Topnav.tsx
@@ -1,16 +1,17 @@
-import { ReactNode } from "react";
+import type { ReactNode } from 'react'
 
 interface Props {
-  title?: string;
-  icon: ReactNode;
+  title?: string
+  icon: ReactNode
 }
-export const TopNav: React.FC<Props> = ({ title = "山竹记账", icon }) => {
+export const TopNav: React.FC<Props> = ({ title = '山竹记账', icon }) => {
   return (
     <div text-white flex items-center pt-24px pb-8px px-24px>
-      <span className="w-24px h-24px mr-16px" flex justify-center items-center>
+      <span w-24px h-24px mr-16px flex justify-center items-center
+        children-max-w="100%" children-max-h="100%">
         {icon}
       </span>
       <h1 text-24px>{title}</h1>
     </div>
-  );
-};
+  )
+}

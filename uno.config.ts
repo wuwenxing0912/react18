b113f627dbe704bc1a5a1a80f5efe5464841a9c7
wuwenxing0912 <1428560819@qq.com>
2024-01-16 11:19:20 +0800

    登录页样式

--- uno.config.ts
+++ uno.config.ts
@@ -1,24 +1,30 @@
 import {
-  defineConfig, presetAttributify, presetIcons,
-  presetTypography, presetUno, transformerAttributifyJsx
-} from 'unocss'
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetTypography,
+  presetUno,
+  transformerAttributifyJsx,
+} from "unocss";
 
 export default defineConfig({
-  theme: {
-  },
+  theme: {},
   shortcuts: {
-    'j-btn': 'h-48px w-100% bg-#5C33BE b-none text-white rounded-8px'
+    "j-btn": "h-48px w-100% bg-#5C33BE b-none text-white text-18px rounded-8px",
+    "j-input-text":
+      "h-48px px-16px leading-32px py-8px b-#5C33BE b-1 focus:shadow focus:shadow-inset rounded-8px text-18px",
+    "j-form":
+      "px-16px flex flex-col gap-y-24px children-flex children-flex-col",
+    "j-form-label": "text-18px mb-8px",
   },
   safelist: [],
   presets: [
     presetUno(),
     presetAttributify(),
     presetIcons({
-      extraProperties: { 'display': 'inline-block', 'vertical-align': 'middle' },
+      extraProperties: { display: "inline-block", "vertical-align": "middle" },
     }),
     presetTypography(),
   ],
-  transformers: [
-    transformerAttributifyJsx()
-  ],
-})
+  transformers: [transformerAttributifyJsx()],
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import Unocss from 'unocss/vite'
-import { viteMockServe } from 'vite-plugin-mock'
-import { svgsprites } from './vite_plugins/svgsprites'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import Unocss from "unocss/vite";
+import { viteMockServe } from "vite-plugin-mock";
+import { svgsprites } from "./vite_plugins/svgsprites";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
   define: {
-    isDev: command === 'serve'
+    isDev: command === "serve",
   },
   plugins: [
     Unocss(),
     react(),
     viteMockServe(),
-    svgsprites({ noOptimizeList: ['chart', 'category', 'export', 'noty'] })
-  ]
-}))
+    svgsprites({
+      noOptimizeList: ["logo", "chart", "category", "export", "noty"],
+    }),
+  ],
+}));

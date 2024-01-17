cfe3c21ce93f50ca36aa54a477e9bdc5f5838fd3
wuwenxing0912 <1428560819@qq.com>
2024-01-17 15:58:03 +0800

    使用 prefixClass 自定义Tabs内部样式

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,20 +1,30 @@
+import type { ReactNode } from 'react'
+import classnames from 'classnames'
 import s from './Tabs.module.scss'
 
 interface Props<T> {
-  tabs: { key: T; text: string }[]
+  tabs: { key: T; text: string; element?: ReactNode }[]
   value: string
   onChange: (key: T) => void
   className: string
+  prefixClass?: string
 }
 
 export const Tabs = <T extends string>(props: Props<T>) => {
-  const { tabs, value, onChange, className } = props
+  const { tabs, value, onChange, className, prefixClass } = props
   return (
-    <ol flex text-white children-px-24px children-py-12px className={className}>
-      {tabs.map(tab => <li key={tab.key} className={tab.key === value ? s.selected : ''}
-        onClick={() => onChange(tab.key)}>
-        {tab.text}
-      </li>)}
-    </ol>
+    <div className={className}>
+      <ol flex text-white children-px-24px children-py-12px bg="[rgb(143,76,215)]"
+        className={prefixClass ? `${prefixClass}-menu` : ''}>
+        {tabs.map(tab => <li key={tab.key}
+          className={classnames(tab.key === value ? s.selected : '', prefixClass ? `${prefixClass}-menu-item` : '')}
+          onClick={() => onChange(tab.key)}>
+          {tab.text}
+        </li>)}
+      </ol>
+      <div className={prefixClass ? `${prefixClass}-pane` : ''}>
+        {tabs.filter(tab => tab.key === value)[0].element}
+      </div>
+    </div>
   )
 }
--- src/pages/ItemsNewPage.tsx
+++ src/pages/ItemsNewPage.tsx
@@ -1,18 +1,26 @@
+import type { ReactNode } from 'react'
 import { useState } from 'react'
 import { Gradient } from '../components/Gradient'
 import { Icon } from '../components/Icon'
 import { Tabs } from '../components/Tabs'
 import { TopNav } from '../components/TopNav'
+import s from './ItemsNewPage.module.scss'
+
+type itemKind = 'income' | 'expenses'
 
 export const ItemsNewPage: React.FC = () => {
-  const tabsItem = [{ key: 'income', text: '收入' }, { key: 'expenses', text: '支出' }]
+  const tabsItem: { key: itemKind; text: string; element?: ReactNode }[] = [
+    { key: 'income', text: '收入', element: <div>收入</div> },
+    { key: 'expenses', text: '支出', element: <div>支出</div> }
+  ]
   const [tab, setTab] = useState('income')
   return (
-    <div>
+    <div className={s['items-new-page-wrapper']}>
       <Gradient>
         <TopNav title="记一笔" icon={<Icon name="back" />} />
-        <Tabs tabs={tabsItem} value={tab} onChange={setTab} className='children-text-center children-flex-1' />
       </Gradient>
+      <Tabs tabs={tabsItem} value={tab} onChange={setTab} prefixClass='items-new-page'
+        className='children-text-center children-flex-1' />
     </div>
   )
 }

2670b194fa64f828f1e60d58369455e5ac03ff40
wuwenxing0912 <1428560819@qq.com>
2024-01-20 11:14:26 +0800

    引入echarts 创建LineChart组件

---
+++ src/components/LineChart.tsx
@@ -0,0 +1,65 @@
+import { useEffect, useRef } from 'react'
+import * as echarts from 'echarts'
+
+type Props = {
+  className?: string
+  items?: { x: number | string; y: number }[]
+}
+export const LineChart: React.FC<Props> = (props) => {
+  const { className, items } = props
+  const div = useRef<HTMLDivElement>(null)
+  const xItems = items?.map(item => item.x)
+  const yItems = items?.map(item => item.y)
+  useEffect(() => {
+    if (!div.current) { return }
+    const myChart = echarts.init(div.current)
+    const option: echarts.EChartsOption = {
+      tooltip: {
+        trigger: 'axis',
+        show: true,
+        formatter: ([{ axisValue, data }]: any) => {
+          const parts = axisValue.split('-')
+          const label = `${parts[0]}年${parts[1]}月${parts[2]}日`
+          const value = data === null ? '无数据' : `${data}元`
+          return `${label}<br/><div style="text-align: right;">${value}</div>`
+        }
+      },
+      grid: {
+        left: 16,
+        top: 8,
+        bottom: 24,
+        right: 16
+      },
+      xAxis: {
+        type: 'category',
+        data: xItems,
+        axisLabel: {
+          formatter: (label: string) => label.slice(label.indexOf('-') + 1)
+        },
+      },
+      yAxis: {
+        type: 'value',
+        axisLabel: {
+          show: false
+        },
+      },
+      series: [
+        {
+          data: yItems,
+          type: 'line',
+          itemStyle: {
+          },
+          emphasis: {
+            itemStyle: {
+              color: 'green'
+            }
+          }
+        }
+      ]
+    }
+    myChart.setOption(option)
+  }, [])
+  return (
+    <div ref={div} className={className}></div>
+  )
+}

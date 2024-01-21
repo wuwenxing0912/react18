29d59d1d21c0c398b0ecababc275bcb864f1bd1d
wuwenxing0912 <1428560819@qq.com>
2024-01-21 19:29:03 +0800

    尝试处理401错误

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import useSWR from 'swr'
-import { Navigate } from 'react-router-dom'
+import { Navigate, useNavigate } from 'react-router-dom'
+import type { AxiosError } from 'axios'
 import p from '../assets/images/pig.svg'
 import { ajax } from '../lib/ajax'
 import { useTitle } from '../hooks/useTitle'
@@ -10,8 +11,17 @@ interface Props {
 }
 export const Home: React.FC<Props> = (props) => {
   useTitle(props.title)
-  const { data: meData, error: meError } = useSWR('/api/v1/me', async path =>
-    (await ajax.get<Resource<User>>(path)).data.resource
+  const nav = useNavigate()
+  const handleHttpError = (error: AxiosError) => {
+    if (error.response?.status === 401) {
+      nav('/sign_in')
+    }
+    throw error
+  }
+  const { data: meData, error: meError } = useSWR('/api/v1/me', async path => {
+    const res = await ajax.get<Resource<User>>(path).catch(handleHttpError)
+    return res?.data?.resource
+  }
   )
   const { data: itemsData, error: itemsError } = useSWR(meData ? '/api/v1/items' : null, async path =>
     (await ajax.get<Resources<Item>>(path)).data
